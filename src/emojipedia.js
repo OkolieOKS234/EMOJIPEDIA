1099c4f8459dc489e7e230e6bc3cb021402eb794
user name <98519572+OkolieOKS234@users.noreply.github.com>
2022-09-12 15:50:10 +0100

    Mapped my component

--- src/emojipedia.js
+++ src/emojipedia.js
@@ -4,20 +4,43 @@ const emojipedia = [
     emoji: "💪",
     name: "Tense Biceps",
     meaning:
-      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym.",
   },
   {
     id: 2,
     emoji: "🙏",
     name: "Person With Folded Hands",
     meaning:
-      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you.",
   },
   {
     id: 3,
     emoji: "🤣",
     name: "Rolling On The Floor, Laughing",
     meaning:
-      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
-  }
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“.",
+  },
+  {
+    id: 4,
+    emoji: "👩‍🌾",
+    name: "Female farmer",
+    meaning:
+      "We are going on a farm holiday! Nature, animals, meadows and fields. The woman in a straw hat and dungarees works in agriculture, grows fruit and vegetables or is nature-loving.",
+  },
+  {
+    id: 5,
+    emoji: "👨‍🍳",
+    name: "Male cook",
+    meaning:
+      "Too many cooks spoil the broth! The man is wearing a chef's jacket as well as a chef's hat and is responsible for the preparation of tasty food. Whereas men are often only able to “cook“ fried eggs at home, you can find more men than women in professional kitchens.",
+  },
+  {
+    id: 6,
+    emoji: "😚",
+    name: "Kissing face with closed eyes",
+    meaning:
+      "Cute face with closed eyes and rosy cheeks. As a thank you for a tip or a favor. Loving kiss to the closest friends, family or darling.",
+  },
 ];
+
+export default emojipedia;
